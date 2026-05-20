a2ed03caac89c24b4560288aeff851e4f86d002c
sujay3824-cell <sujay.30082004@gmail.com>
2026-05-20 20:29:36 +0530

    Added RFID auto login with serial integration

--- serial_reader.py
+++ serial_reader.py
@@ -1,7 +1,9 @@
 import serial
 import requests
+import json
+import webbrowser
 
-# COM Port
+# COM PORT
 ser = serial.Serial('COM14', 115200)
 
 server_url = "https://rfid-jyxq.onrender.com/rfid_auto"
@@ -12,20 +14,48 @@ while True:
 
     if ser.in_waiting:
 
-        uid = ser.readline().decode().strip()
-
-        print("Scanned UID:", uid)
-
-        data = {
-            "rfid": uid
-        }
-
         try:
 
+            raw_data = ser.readline().decode(errors='ignore').strip()
+
+            # Print raw serial data
+            print("RAW:", raw_data)
+
+            # Only process JSON RFID data
+            if not raw_data.startswith('{"rfid"'):
+                continue
+
+            # Convert JSON string to dictionary
+            data = json.loads(raw_data)
+
+            print("Valid RFID:", data)
+
+            # Ignore logout clear signal
+            if data["rfid"] == "CLEAR":
+                print("Logout")
+                continue
+
+            # Send to Flask server
             response = requests.post(server_url, json=data)
 
             print("Server Response:", response.status_code)
-            print(response.text)
+
+            result = response.json()
+
+            print(result)
+
+            # Open dashboard automatically
+            if "redirect" in result:
+
+                dashboard_url = "https://rfid-jyxq.onrender.com/dashboard/doctor" + result["redirect"]
+
+                print("Opening:", dashboard_url)
+
+                webbrowser.open(dashboard_url)
+
+            else:
+
+                print("RFID not found in database")
 
         except Exception as e:
 
